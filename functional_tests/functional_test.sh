b5b07fafb8edaf40a8a648fd219c8f68c36358b6
VidsSkids <victor.massonnat@epitech.eu>
2023-02-12 14:39:37 +0100

    fix: building of the project for functional test

--- functional_tests/functional_test.sh
+++ functional_tests/functional_test.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-make
-./GinGen $(pwd)/functional_tests/file_test.go $(pwd)/functional_tests/output_test.json
+./GinGen $(pwd)/functional_tests/testing_file $(pwd)/functional_tests/output_test.json
 diff $(pwd)/functional_tests/output_test.json $(pwd)/functional_tests/default_output.json
