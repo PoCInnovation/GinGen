92e12965062abff729e599453f46c47027b18589
VidsSkids <victor.massonnat@epitech.eu>
2023-02-12 14:50:03 +0100

    fix: typo error and remove useless shell script

--- functional_tests/functional_test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-./GinGen $(pwd)/functional_tests/testing_file $(pwd)/functional_tests/output_test.json
-diff $(pwd)/functional_tests/output_test.json $(pwd)/functional_tests/default_output.json
